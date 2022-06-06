da15edcd3cdc47831c43b4fe3d642c92befb0c7d
Rumi Humphrey <rumi@neomantra.net>
2022-06-06 14:34:09 -0400

    Add header back

--- functions/test.ts
+++ functions/test.ts
@@ -1,21 +1,10 @@
-// export async function onRequest(context:any) {
-//   // Contents of context object
-//   const {
-//     request, // same as existing Worker API
-//     env, // same as existing Worker API
-//     params, // if filename includes [id] or [[path]]
-//     waitUntil, // same as ctx.waitUntil in existing Worker API
-//     next, // used for middleware or to fetch assets
-//     data, // arbitrary space for passing data between middlewares
-//   } = context;
-
-//   return new Response("Hello, world!");
-// }
-
-
 export async function onRequestGet({ params }) {
   const res = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`);
   const data = await res.json();
   const info = JSON.stringify(data, null, 2);
-  return new Response(info);
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Content-type': 'application/json',
+  };
+  return new Response(info, { headers });
 }
