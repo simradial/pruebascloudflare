c362ef19a46a7fa49825bd6e257b9d2189f1d628
Rumi Humphrey <rumi@neomantra.net>
2022-06-06 20:41:40 -0400

    shuffle frontend

---
+++ functions/character/[id].ts
@@ -0,0 +1,17 @@
+// export async function onRequestGet({ params }) {
+//   const res = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`);
+//   const data = await res.json();
+//   //const info = JSON.stringify(data, null, 2);
+//   const headers = {
+//     'Access-Control-Allow-Origin': '*',
+//     'Content-type': 'application/json',
+//   };
+//   return new Response(data, { headers });
+// }
+
+export async function onRequestGet({ params }) {
+  const res = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`);
+  const data = await res.json();
+  const info = JSON.stringify(data, null, 2);
+  return new Response(info);
+}
